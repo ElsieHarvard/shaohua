f57b7ca0bc1ddb7c7f58991e05050ab032b7c8d5
Elsie Harvard <elsieharvard@qq.com>
2015-02-22 11:21:41 +0800

    NON-A

--- app/controllers/web_global_controller.rb
+++ app/controllers/web_global_controller.rb
@@ -1,7 +1,31 @@
 class WebGlobalController < ApplicationController
   def mainsite_navigator
   end
-  def usrlogin
-  	render status: 200, plain: params.inspect
+  def usrlogin # Not Safe
+    usr=WebAccount.find_by(usrname: firewall(params[:usrinfo][:usrname]))
+    if usr
+      pas=firewall(params[:usrinfo][:usrpassword])
+      slt=usr.password_salt.unpack('m')[0]
+      itr=(usr.usrtype=='admin' ? 65536 : 16384)
+      psh=usr.usrpasswordhash
+      hsh=[OpenSSL::PKCS5.pbkdf2_hmac(pas,slt,itr,512,OpenSSL::Digest::SHA512.new)].pack('m')
+      if psh == hsh
+        render plain: 'true'
+      else
+        render plain: 'false'
+      end
+    else
+      render plain: 'false'
+    end
+  end
+  def publickey
+    return OpenSSL::PKey::RSA.new 
+  end
+  private
+  def firewall(src)
+    return src.gsub(/\W/,'')
+  end
+  def firewall_hex(src)
+    return src.gsub(/\H/,'')
   end
 end
