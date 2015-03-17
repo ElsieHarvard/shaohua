17ea56ebd8fc7e94b6d192d2dc30a2460257bcc4
Elsie Harvard <elsieharvard@qq.com>
2015-03-17 12:53:44 +0800

    prdtst

--- app/helpers/periodicals_helper.rb
+++ app/helpers/periodicals_helper.rb
@@ -1,2 +1,20 @@
 module PeriodicalsHelper
+  def getarcsh(hsh)
+    # raise hsh
+    if /\h{64}/.match hsh
+      if PeriodicalArticle.exists?(:archash=>hsh.downcase.chomp)
+        return PeriodicalArticle.find_by(archash:hsh.downcase.chomp)
+      else
+        raise '404'
+      end
+    elsif /\h{39}/.match hsh
+      if Article.exists?(:archash=>hsh.downcase.chomp)
+        return Article.arcfind(hsh.downcase.chomp)
+      else
+        raise '404'
+      end
+    else
+      raise 'BadArcHsh'
+    end
+  end
 end
--- app/models/periodical.rb
+++ app/models/periodical.rb
@@ -6,4 +6,7 @@ class Periodical < ActiveRecord::Base
 		write_attribute :prdpages,prdpages.gsub('<','&lt;').gsub('>','&gt;')
 		write_attribute :prdhash,OpenSSL::Digest::SHA1.hexdigest(prdtitle+prdinfo+prdmsg+prdpages+prdno.to_s)
 	end
+	def self.top(num)
+		return self.order(:prdno).last(num).reverse
+	end
 end
