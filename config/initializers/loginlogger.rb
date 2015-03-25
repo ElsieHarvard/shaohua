599f3fed562558719943d26c65e3bf4534c07e1b
Elsie Harvard <ElsieHarvard@users.noreply.github.com>
2015-03-25 05:29:11 +0000

    Update loginlogger.rb

--- config/initializers/loginlogger.rb
+++ config/initializers/loginlogger.rb
@@ -1,3 +1,8 @@
+class LoginLogger < Logger
+  def format_message(severity, timestamp, progname, msg)
+    "#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
+  end
+end
 login_logfile = File.open("#{Rails.root}/log/login.log", 'a')
 login_logfile.sync = true
 LOGIN_LOG = LoginLogger.new(login_logfile)
